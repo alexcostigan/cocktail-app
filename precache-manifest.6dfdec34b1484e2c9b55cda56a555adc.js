c0cb7900cc647b74f9cee9a1037a34bd93fec9df
alexcostigan <alexcostigan@icloud.com>
2020-06-04 19:07:59 +0100

    Updates

---
+++ precache-manifest.6dfdec34b1484e2c9b55cda56a555adc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407a845def10769232954e9b99daaa8f",
+    "url": "/cocktail-app/index.html"
+  },
+  {
+    "revision": "553428be100727700995",
+    "url": "/cocktail-app/static/css/main.081cbfa3.chunk.css"
+  },
+  {
+    "revision": "39a2cbf97e5ff1c8c777",
+    "url": "/cocktail-app/static/js/2.ac3a4bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cocktail-app/static/js/2.ac3a4bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553428be100727700995",
+    "url": "/cocktail-app/static/js/main.7a9afb40.chunk.js"
+  },
+  {
+    "revision": "66aebf48f20625df8925",
+    "url": "/cocktail-app/static/js/runtime-main.d757759b.js"
+  },
+  {
+    "revision": "e444a20239dfa9d302a21a76d8ccea80",
+    "url": "/cocktail-app/static/media/cocktail-shaker.e444a202.gif"
+  }
+]);
